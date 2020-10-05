d611040e0ef8b5df68626f58603f00a4d1a5df9f
vcw <byzov.iv@gmail.com>
2020-10-05 12:08:16 +0000

    Add navbar to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,28 @@
 <template>
   <div>
-    <Nuxt />
+    <vs-navbar
+      fixed
+      shadow
+    >
+    <template #right>
+      <Auth />
+    </template>
+    </vs-navbar>
+    <div class="layout-container">
+      <Nuxt />
+    </div>
   </div>
 </template>
+
+<script>
+import Auth from '~/components/Auth.vue'
+
+export default {
+  components: {
+    Auth,
+  },
+}
+</script>
 
 <style>
 html {
@@ -31,32 +51,11 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+body {
+  background-color: #eee;
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.layout-container {
+  margin-top: 50px;
 }
 </style>
