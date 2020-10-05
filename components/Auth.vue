db0e1f8ee1b02c65d08a86222e617675a9728a79
vcw <byzov.iv@gmail.com>
2020-10-05 12:07:33 +0000

    Add auth component

---
+++ components/Auth.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <vs-button
+      v-if="!$store.state.auth.user"
+      @click.prevent="isLoginPopupActive = !isLoginPopupActive"
+    >
+      Войти
+    </vs-button>
+    <vs-button
+      v-if="$store.state.auth.user"
+      :loading="loggingOut"
+      @click.prevent="logout"
+    >
+      Выйти
+    </vs-button>
+    <LoginPopup v-model="isLoginPopupActive" />
+  </div>
+</template>
+
+<script>
+import LoginPopup from '~/components/LoginPopup.vue'
+
+export default {
+  components: {
+    LoginPopup,
+  },
+  data: function () {
+    return {
+      isLoginPopupActive: false,
+      loggingOut: false,
+    }
+  },
+  methods: {
+    async logout () {
+      this.loggingOut = true
+      await this.$fireAuth.signOut()
+      this.loggingOut = false
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ components/LoginPopup.vue
@@ -0,0 +1,135 @@
+<template>
+  <vs-dialog v-model="dialog">
+    <template #header>
+      <strong>Войти</strong>
+    </template>
+    <form class="form">
+      <vs-input
+        label-placeholder="E-mail"
+        v-model="email"
+        type="email"
+        block
+      />
+
+      <vs-input
+        label-placeholder="Пароль"
+        v-model="password"
+        type="password"
+        block
+      />
+
+      <vs-button
+        v-if="!toggleSignUp"
+        block
+        @click.prevent="onLoginButtonClick"
+        :loading="loading"
+      >
+        Войти
+      </vs-button>
+    </form>
+
+    <vs-button
+      v-if="toggleSignUp"
+      block
+      @click.prevent="onLoginButtonClick"
+      :loading="loading"
+    >
+      Зарегистрироваться {{user}}
+    </vs-button>
+
+    <template #footer>
+      <div class="footer-dialog">
+
+        <div v-if="!toggleSignUp" class="new">
+          Впервые здесь?
+          <a href="#" @click.prevent="toggleSignUp = true">Регистрация</a>
+        </div>
+
+        <div v-if="toggleSignUp" class="new">
+          Уже зарегистрированы?
+          <a href="#" @click.prevent="toggleSignUp = false">Вход</a>
+        </div>
+      </div>
+    </template>
+  </vs-dialog>
+</template>
+
+<script>
+  import { mapState } from 'vuex';
+
+  export default {
+    props: {
+      value: Boolean,
+    },
+    data: () => ({
+      email: 'test@test.org',
+      password: 'testtest',
+      toggleSignUp: false,
+      loading: false,
+    }),
+    computed: {
+      ...mapState({
+        user: state => state.auth.user
+      }),
+      dialog: {
+        get () {
+          return this.value;
+        },
+        set (val) {
+          this.$emit('input', val)
+        }
+      }
+    },
+    methods: {
+      async onLoginButtonClick() {
+        try {
+          this.loading = true
+          await this.$fireAuth.signInWithEmailAndPassword(this.email, this.password)
+          this.loading = false
+          this.dialog = false
+          console.log(this.$store.state.auth.user)
+
+        } catch (error) {
+          console.log(error)
+        }
+      },
+
+      async onSignUpButtonClick() {
+        try {
+          await this.$fireAuth.createUserWithEmailAndPassword(
+            this.email,
+            this.password
+          )
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    },
+    watch: {
+      user(authUserObj) {
+        if (authUserObj) {
+          this.$vs.notification({
+            title: 'Успех!',
+            text: `Вы вошли как ${authUserObj.email}`
+          })
+        } else if (authUserObj === null) {
+          this.$vs.notification({
+            title: 'До встречи!',
+            text: 'Вы вышли из аккаунта'
+          })
+        }
+
+      }
+    }
+  }
+</script>
+
+<style>
+.form {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 20vh;
+
+}
+</style>
