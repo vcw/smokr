91045fb9c81523c0be69a6590180e19352db84d5
vcw <byzov.iv@gmail.com>
2020-10-06 05:21:30 +0000

    Expose firebase config + Active firestore functionality

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 
+import firebaseConfig from './firebase.config.js';
+
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
@@ -50,15 +52,7 @@ export default {
   axios: {},
 
   firebase: {
-    config: {
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      databaseURL: process.env.FIREBASE_DATABASE_URL,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.FIREBASE_APP_ID,
-    },
+    config: firebaseConfig,
     services: {
       auth: {
         persistence: 'local',
