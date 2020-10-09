f3a95a5bc7eba99ccbb081793dd49fa0855f47cb
Ilia Byzov <eliot@Ilias-MacBook-Air.local>
2020-10-09 08:54:59 +0300

    Add displaying last smoke in dashboard

---
+++ components/ui/sCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+.card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 1rem;
+
+  width: 100%;
+  height: 10rem;
+  
+  border-radius: .6rem;
+
+  background-color: #fff;
+
+  box-shadow: 0px 5px 25px 0px rgba(0, 0, 0, .1);
+
+}
+</style>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,24 +1,40 @@
 <template>
   <div class="dashboard">
     <div class="dashboard__actions">
-      <vs-button
-      @click.prevent="smoke"
-      :loading="doSmokeLoading"
-      size="xl"
-      success
-      >
-        Совершить курение
-      </vs-button>
+      <s-card class="dashboard__card">
+        Последнее курение: {{ lastSmoke.getHours() }}:{{ lastSmoke.getMinutes() }}
+
+        <vs-button
+        @click.prevent="smoke"
+        :loading="doSmokeLoading"
+        size="xl"
+        success
+        >
+          Совершить курение
+        </vs-button>
+      </s-card>
+      
     </div>
   </div>
 </template>
 
 <script>
+import sCard from '~/components/ui/sCard.vue'
+import { mapState } from 'vuex'
+
 export default {
+  components: {
+    sCard
+  },
   data () {
     return {
       doSmokeLoading: false
     }
+  },
+  computed: {
+    ...mapState({
+      lastSmoke: (state) => state.userData.lastSmoke.timestamp
+    })
   },
   methods: {
     async smoke () {
@@ -38,5 +54,9 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.dashboard__card {
+
 }
 </style>
