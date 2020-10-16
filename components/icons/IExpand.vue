04007e6457676a534a591b4ded116e7ced89c482
vcw <byzov.iv@gmail.com>
2020-10-16 17:48:57 +0000

    Remove unused class

--- components/icons/IExpand.vue
+++ components/icons/IExpand.vue
@@ -1,13 +1,11 @@
 <template>
-  <svg v-bem class="expand-icon" viewBox="0 0 12 12">
+  <svg v-bem viewBox="0 0 12 12">
     <line
-      class="expand-icon__left"
       x1="2" y1="4"
       x2="6.5" y2="8"
       stroke="black" stroke-width="2" stroke-linecap="round"
     />
     <line
-      class="expand-icon__right"
       x1="6.5" y1="8"
       x2="10" y2="4"
       stroke="black" stroke-width="2" stroke-linecap="round"
