6803fd5458713f0e48c31e3e4e0853d69ed6a87e
vcw <byzov.iv@gmail.com>
2020-10-18 05:41:34 +0000

    Add close icon

---
+++ components/icons/IClose.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg :class="b()" viewBox="0 0 10 10">
+    <line x1="2" y1="2" x2="8" y2="8" :stroke="color" stroke-width="2" stroke-linecap="round" />
+    <line x1="8" y1="2" x2="2" y2="8" :stroke="color" stroke-width="2" stroke-linecap="round" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'IClose',
+  props: {
+    color: {
+      default: '#000',
+      type: String,
+    },
+  },
+};
+</script>
+
+<style>
+.i-close {
+  display: block;
+}
+</style>
