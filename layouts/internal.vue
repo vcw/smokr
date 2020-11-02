2cf271343035fc34e76409d76a49c7fdce4c2b5c
vcw <byzov.iv@gmail.com>
2020-11-02 16:32:08 +0000

    Add internal layout

---
+++ layouts/internal.vue
@@ -0,0 +1,34 @@
+<template>
+  <div :class="b()">
+    <title-bar />
+    <main :class="b('content')">
+      <Nuxt />
+    </main>
+    <nav-bar />
+  </div>
+</template>
+
+<script>
+import TitleBar from '~/components/TitleBar.vue';
+import NavBar from '~/components/NavBar.vue';
+
+export default {
+  name: 'InternalLayout',
+  components: {
+    TitleBar,
+    NavBar,
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;700&display=swap');
+
+.internal-layout {
+  font-family: 'Ubuntu', sans-serif;
+}
+
+.internal-layout__content {
+  margin: 5rem 1rem;
+}
+</style>
