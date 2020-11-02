fbd7501ee6087c3f51f6a7995dd768f1957a4586
vcw <byzov.iv@gmail.com>
2020-11-02 16:31:56 +0000

    Add navigation store module

---
+++ store/navigation.js
@@ -0,0 +1,40 @@
+const mutations = {
+  SET_PAGE_TITLE(state, title) {
+    state.pageTitle = title;
+  },
+  SET_BACK_PATH(state, path) {
+    state.backPath = path;
+  },
+  SET_SHOW_NAVBAR(state, navbar) {
+    state.showNavBar = navbar;
+  },
+  CLEAR_NAVIGATION(state) {
+    state.pageTitle = null;
+    state.backPath = null;
+  },
+};
+
+const actions = {
+  init({ commit }, { title, back, navbar }) {
+    commit('CLEAR_NAVIGATION');
+    commit('SET_PAGE_TITLE', title);
+    if (back) {
+      commit('SET_BACK_PATH', back);
+    }
+    if (navbar) {
+      commit('SET_SHOW_NAVBAR', navbar);
+    }
+  },
+};
+
+const state = () => ({
+  pageTitle: null,
+  backPath: null,
+  showNavBar: false,
+});
+
+export default {
+  state,
+  mutations,
+  actions,
+};
