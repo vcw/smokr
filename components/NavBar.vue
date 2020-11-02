c551d7c5456dd9c4665c9131f9b046b2495c4302
vcw <byzov.iv@gmail.com>
2020-11-02 16:31:33 +0000

    Add navigation and title bars

---
+++ components/NavBar.vue
@@ -0,0 +1,83 @@
+<template>
+  <nav v-if="show" :class="b()">
+    <router-link
+      :class="b('link', { active: path === '/home' })"
+      to="/home"
+    >
+      <i-home :class="b('icon')" />
+    </router-link>
+
+    <router-link
+      :class="b('link', { active: path === '/stats' })"
+      to="/stats"
+    >
+      <i-stats :class="b('icon')" />
+    </router-link>
+
+    <router-link
+      :class="b('link', { active: path === '/settings' })"
+      to="/settings"
+    >
+      <i-settings
+        :class="b('icon')"
+        color=""
+      />
+    </router-link>
+  </nav>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import IHome from '~/components/icons/IHome.vue';
+import IStats from '~/components/icons/IStats.vue';
+import ISettings from '~/components/icons/ISettings.vue';
+
+export default {
+  name: 'NavBar',
+  components: {
+    IHome,
+    IStats,
+    ISettings,
+  },
+  computed: {
+    ...mapState({
+      show: (state) => state.navigation.showNavBar,
+    }),
+    path() {
+      return this.$route.path;
+    },
+  },
+};
+</script>
+
+<style>
+.nav-bar {
+  display: flex;
+  justify-content: space-around;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  padding: .5rem;
+  width: 100%;
+  background: #E9E9E9;
+  border-top: .1rem solid #495867;
+}
+
+.nav-bar__link {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 3rem;
+  height: 3rem;
+  border-radius: .5rem;
+}
+
+.nav-bar__link_active {
+  background: #CCC;
+}
+
+.nav-bar__icon {
+  width: 2rem;
+}
+</style>
---
+++ components/TitleBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <header
+    v-if="title"
+    :class="b()"
+  >
+    <router-link
+      v-if="back"
+      :to="back"
+    >
+      <i-arrow color="#495867" />
+    </router-link>
+
+    <h1 :class="b('title')">
+      {{ title }}
+    </h1>
+  </header>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import IArrow from '~/components/icons/iArrow.vue';
+
+export default {
+  name: 'TitleBar',
+  components: {
+    IArrow,
+  },
+  computed: {
+    ...mapState({
+      title: (state) => state.navigation.pageTitle,
+      back: (state) => state.navigation.backPath,
+    }),
+  },
+};
+</script>
+
+<style>
+.title-bar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: grid;
+  grid-template-columns: 1.25rem 1fr;
+  align-items: center;
+  gap: 0.375rem;
+  padding: 0.375rem;
+  height: 3.75rem;
+  width: 100%;
+  background: #fff;
+  box-shadow: 0rem .25rem .5rem rgba(0, 0, 0, 0.05);
+}
+
+.title-bar__title {
+  grid-column: 2 / 3;
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: #495867;
+}
+</style>
