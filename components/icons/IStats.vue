daa96bcbd64cb49d8caaf203b927554d4ef3e1db
vcw <byzov.iv@gmail.com>
2020-11-02 16:30:47 +0000

    Add icons

---
+++ components/icons/IStats.vue
@@ -0,0 +1,28 @@
+/* eslint-disable max-len */
+<template>
+  <svg
+    :class="b()"
+    :fill="color"
+    viewBox="0 0 102 102"
+  >
+    <path d="M71.059 78.754A3.187 3.187 0 0076.5 76.5V51a3.187 3.187 0 10-6.375 0v25.5c0 .845.336 1.656.934 2.254zM48.746 78.754a3.187 3.187 0 005.441-2.254v-51a3.187 3.187 0 10-6.374 0v51c0 .845.335 1.656.933 2.254zM26.434 78.754a3.187 3.187 0 005.441-2.254V38.25a3.187 3.187 0 10-6.375 0V76.5c0 .845.336 1.656.934 2.254z" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'IStats',
+  props: {
+    color: {
+      type: String,
+      default: '#000',
+    },
+  },
+};
+</script>
+
+<style>
+.i-stats {
+  display: block;
+}
+</style>
---
+++ components/icons/iArrow.vue
@@ -0,0 +1,47 @@
+<template>
+  <svg
+    :class="b()"
+    viewBox="0 0 20 20"
+    fill="none"
+    :stroke="color"
+    stroke-width="3"
+    :transform="`rotate(${rotate})`"
+  >
+    <path
+      stroke-linecap="round"
+      d="M13.3333 3.33337L6.66665 10M6.66665 10L13.3333 16.6667"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'IArrow',
+  props: {
+    direction: {
+      type: String,
+      default: 'left',
+    },
+    color: {
+      type: String,
+      default: '#000',
+    },
+  },
+  data() {
+    return {
+      rotate: {
+        left: '0',
+        top: '90',
+        right: '180',
+        bottom: '270',
+      }[this.direction],
+    };
+  },
+};
+</script>
+
+<style>
+.i-arrow {
+  display: block;
+}
+</style>
