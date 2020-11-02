cf9d1df42669ea3b851c49e6147a15ceb954e1e8
vcw <byzov.iv@gmail.com>
2020-11-02 17:08:35 +0000

    Add home page

---
+++ pages/home.vue
@@ -0,0 +1,36 @@
+<template>
+  <s-card
+    :class="b()"
+  >
+    <last-smoking :class="b('last-smoking')" />
+    <add-smoking />
+  </s-card>
+</template>
+
+<script>
+import LastSmoking from '~/components/LastSmoking.vue';
+import AddSmoking from '~/components/AddSmoking.vue';
+import SCard from '~/components/ui/SCard.vue';
+
+export default {
+  name: 'HomePage',
+  components: {
+    LastSmoking,
+    AddSmoking,
+    SCard,
+  },
+  layout: 'internal',
+  beforeMount() {
+    this.$store.dispatch('navigation/init', {
+      title: 'Главная',
+      navbar: true,
+    });
+  },
+};
+</script>
+
+<style>
+.home-page__last-smoking {
+  margin-bottom: 1rem;
+}
+</style>
