84bcf5c7759598d71c0e25130fcd978ba0913f2f
vcw <byzov.iv@gmail.com>
2020-11-02 17:08:45 +0000

    Add sign-in page

---
+++ pages/sign-in.vue
@@ -0,0 +1,31 @@
+<template>
+  <s-card>
+    <login-registration-form :class="b('form')" />
+  </s-card>
+</template>
+
+<script>
+import SCard from '~/components/ui/SCard.vue';
+import LoginRegistrationForm from '~/components/LoginRegistrationForm.vue';
+
+export default {
+  name: 'SignInPage',
+  components: {
+    SCard,
+    LoginRegistrationForm,
+  },
+  layout: 'internal',
+  beforeMount() {
+    this.$store.dispatch('navigation/init', {
+      title: 'Вход',
+      back: '/',
+    });
+  },
+};
+</script>
+
+<style>
+.sign-in-page__form {
+  width: 100%;
+}
+</style>
