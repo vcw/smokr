6d11ae0c1687a4b23263bfb26c8d420495778a1f
vcw <vcw@users.noreply.github.com>
2020-11-03 20:15:58 +0300

    Add Stats page

---
+++ pages/stats.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <stats />
+  </div>
+</template>
+
+<script>
+import Stats from '~/components/Stats.vue';
+
+export default {
+  components: {
+    Stats,
+  },
+  layout: 'internal',
+  beforeMount() {
+    this.$store.dispatch('navigation/init', {
+      title: 'Статистика',
+    });
+  },
+};
+</script>
